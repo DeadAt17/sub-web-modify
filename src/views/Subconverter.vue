31c4f19eb34c68b3292e43083871c28c5059615f
Eddie Peng <Eddie.JJ.Peng@pccw.com>
2025-02-19 13:26:55 +0800

    update

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -1007,12 +1007,12 @@ export default {
           .then(res => {
             this.backendVersion = res.data.replace(/backend\n$/gm, "");
             this.backendVersion = this.backendVersion.replace("subconverter", "SubConverter");
-            let a = this.form.customBackend.indexOf("url.v1.mk") !== -1 || this.form.customBackend.indexOf("sub.d1.mk") !== -1;
+            let a = this.form.customBackend.indexOf("sub.delta17.top") !== -1 || this.form.customBackend.indexOf("sub.delta17.top") !== -1;
             let b = this.form.customBackend.indexOf("127.0.0.1") !== -1;
             a ? this.$message.success(`${this.backendVersion}` + "肥羊负载均衡增强版后端，已屏蔽免费节点池（会返回403），额外支持vless reality+hysteria+hysteria2订阅转换") : b ? this.$message.success(`${this.backendVersion}` + "本地局域网自建版后端") : this.$message.success(`${this.backendVersion}` + "官方原版后端不支持vless/hysteria订阅转换");
           })
           .catch(() => {
-            this.$message.error("请求SubConverter版本号返回数据失败，该后端不可用！");
+            this.$message.info("自用 subconverter 后端，库来自 asdlokj1qpi233/subconverter");
           });
     }
   }
